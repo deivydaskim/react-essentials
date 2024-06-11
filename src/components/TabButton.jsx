fefb11fed4101a11dbc22fdaf865ba9a4a1c6e41
deivydas.kim <deivydas.kim@gmail.com>
2024-06-11 20:28:27 +0300

    optimized

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,8 +1,8 @@
 // Native 'children' prop - getting value from <TabButton>value</TabButton>
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
     </li>
   );
 }
